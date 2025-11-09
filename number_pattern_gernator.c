fc4fdd729b3a1d64754142f4da8ec0cabf0682bf
SUNNY PRAKASH <sunnyprakash2348@gmail.com>
2025-11-09 09:16:44 -0800

    number pattern

---
+++ number_pattern_gernator.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int x,k,y;
+    printf("Enter your number for making pattern:");
+    scanf("%d",&k);
+    for(x=1;x<=k;x++){
+        for(y=1;y<=x;y++){
+            printf("%d",x);
+        }
+    printf("\n");   
+    }
+}
